4d02ee034265899492414efeedd939f0d4237356
Nate Roberts <naterob316@gmail.com>
2022-10-23 18:21:44 -0400

    completed toCSV

--- src/nested.ts
+++ src/nested.ts
@@ -100,7 +100,14 @@ id,name,options,points,published
  * Check the unit tests for more examples!
  */
 export function toCSV(questions: Question[]): string {
-    return "";
+    let CSVConversion = "id,name,options,points,published\n";
+    CSVConversion += questions
+        .map(
+            (question: Question): string =>
+                `${question.id},${question.name},${question.options.length},${question.points},${question.published}`
+        )
+        .join("\n");
+    return CSVConversion;
 }
 
 /**
