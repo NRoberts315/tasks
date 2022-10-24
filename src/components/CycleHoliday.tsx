26b396a019ba7fad1a44c3c7215acb513d4eab7d
Nate Roberts <naterob316@gmail.com>
2022-10-24 14:17:51 -0400

    Completed change holiday

--- src/components/CycleHoliday.tsx
+++ src/components/CycleHoliday.tsx
@@ -2,5 +2,40 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function CycleHoliday(): JSX.Element {
-    return <div>Cycle Holiday</div>;
+    //In order, Christmas, Halloween, Thanksgiving, Independence Day (Windows please add flag emoji), Presidents Day
+    type Holiday = "🎄" | "🎃" | "🦃" | "🎆" | "🤵";
+
+    const changeByAlphabet: Record<Holiday, Holiday> = {
+        "🎄": "🎃",
+        "🎃": "🎆",
+        "🎆": "🤵",
+        "🤵": "🦃",
+        "🦃": "🎄"
+    };
+
+    const changeByDay: Record<Holiday, Holiday> = {
+        "🤵": "🎆",
+        "🎆": "🎃",
+        "🎃": "🦃",
+        "🦃": "🎄",
+        "🎄": "🤵"
+    };
+
+    const [holiday, setHoliday] = useState<Holiday>("🎄");
+
+    function incrementByDay(): void {
+        setHoliday(changeByDay[holiday]);
+    }
+
+    function incrementByAlphabet(): void {
+        setHoliday(changeByAlphabet[holiday]);
+    }
+
+    return (
+        <div>
+            <div>Holiday: {holiday}</div>
+            <Button onClick={incrementByAlphabet}>Advance by Alphabet</Button>
+            <Button onClick={incrementByDay}>Advance by Year</Button>
+        </div>
+    );
 }
