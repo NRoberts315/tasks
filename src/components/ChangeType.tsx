488f1ee5ac3bcf6870b1cc8642ca86221652f26f
Nate Roberts <naterob316@gmail.com>
2022-10-24 13:09:50 -0400

    Added change type button functionality

--- src/components/ChangeType.tsx
+++ src/components/ChangeType.tsx
@@ -1,7 +1,32 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
-import { QuestionType } from "../interfaces/question";
+import { Question, QuestionType } from "../interfaces/question";
 
 export function ChangeType(): JSX.Element {
-    return <div>Change Type</div>;
+    type QuestionType = "multiple_choice_question" | "short_answer_question";
+
+    const typeSwap: Record<QuestionType, QuestionType> = {
+        short_answer_question: "multiple_choice_question",
+        multiple_choice_question: "short_answer_question"
+    };
+
+    const [questionType, setQuestionType] = useState<QuestionType>(
+        "short_answer_question"
+    );
+
+    function changeType(): void {
+        const newQuestionType = typeSwap[questionType];
+        setQuestionType(newQuestionType);
+    }
+
+    return (
+        <div>
+            <Button onClick={changeType}>Change Type</Button>
+            {questionType === "short_answer_question" ? (
+                <span>Short Answer</span>
+            ) : (
+                <span>Multiple Choice</span>
+            )}
+        </div>
+    );
 }
