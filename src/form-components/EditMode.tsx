24eb947876658a1424595bf092a66d59905bca32
Nate Roberts <naterob316@gmail.com>
2022-11-20 18:25:45 -0500

    Completed edit mode functionality

--- src/form-components/EditMode.tsx
+++ src/form-components/EditMode.tsx
@@ -1,9 +1,50 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function EditMode(): JSX.Element {
+    const [edit, setEdit] = useState<boolean>(false);
+    const [name, setName] = useState<string>("Your Name");
+    const [isStudent, setIsStudent] = useState<boolean>(true);
+
+    function checkEdit(event: React.ChangeEvent<HTMLInputElement>) {
+        setEdit(event.target.checked);
+    }
+
+    function checkStudent(event: React.ChangeEvent<HTMLInputElement>) {
+        setIsStudent(event.target.checked);
+    }
+
+    function resetName(event: React.ChangeEvent<HTMLInputElement>) {
+        setName(event.target.value);
+    }
+
     return (
         <div>
-            <h3>Edit Mode</h3>
+            <Form.Group>
+                <Form.Check
+                    type="switch"
+                    label="Edit"
+                    checked={edit}
+                    onChange={checkEdit}
+                />
+                <Form.Check
+                    hidden={edit == false}
+                    type="checkbox"
+                    id="isStudent"
+                    label="Is student?"
+                    name="isStudent"
+                    checked={isStudent}
+                    onChange={checkStudent}
+                />
+                <Form.Control
+                    hidden={edit == false}
+                    value={name}
+                    onChange={resetName}
+                />
+            </Form.Group>
+            <h3 hidden={edit == true}>
+                {name} is {isStudent ? "" : "not"} a student
+            </h3>
         </div>
     );
 }
